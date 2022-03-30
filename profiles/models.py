345d6bfcff15c3e2631b2d4ece3bc1da3ee42403
mac <pro@MacBook-Pro-mac-414.local>
2022-03-30 09:33:37 +0300

    profiles is in deal

---
+++ profiles/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Profile(models.Model):
+    status = models.ForeignKey('users.CustomUser', to_field='status', related_name='status')
+
+    class Meta:
+     fields = '__all__'
